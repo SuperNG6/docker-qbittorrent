5ea555adbb5befc229b23b0bc85fe16d9d6115d4
SuperNG6 <sleele@gmail.com>
2021-02-04 16:21:37 +0800

    test

--- .github/workflows/cross_build.sh
+++ .github/workflows/cross_build.sh
@@ -203,9 +203,7 @@ make install
 unset LIBS CPPFLAGS
 
 # build qbittorrent
-wget --no-check-certificate https://github.com/qbittorrent/qBittorrent/archive/release-${ReleaseTag}.tar.gz
-tar -xzf release-${ReleaseTag}.tar.gz
-cd ./qBittorrent-release-${ReleaseTag}
+cd "${SELF_DIR}/../../"
 if [ "${TARGET_HOST}" = 'win' ]; then
   find \( -name '*.cpp' -o -name '*.h' \) -type f -print0 |
     xargs -0 -r sed -i 's/Windows\.h/windows.h/g;
