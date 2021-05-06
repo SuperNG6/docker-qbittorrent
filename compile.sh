ddb11b518db485f657bad40ae132e7b9d2d8065d
SuperNG6 <sleele@gmail.com>
2021-05-06 22:38:45 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -8,7 +8,8 @@ git clone https://github.com/arvidn/libtorrent.git
 cd libtorrent
 git checkout libtorrent-${LIBTV}
 ./autotool.sh
-./configure --disable-debug --enable-encryption
+./configure --disable-debug --enable-encryption --with-boost-libdir=/lib/x86_64-linux-gnu
+make clean && make -j$(nproc)
 make install-strip
 
 # compile qbittorrent
@@ -17,6 +18,7 @@ git clone https://github.com/qbittorrent/qBittorrent
 cd qBittorrent
 git checkout release-${QBV}
 ./configure CXXFLAGS="-std=c++14" --disable-gui --disable-debug
+make clean && make -j$(nproc)
 make install
 
 # packing qbittorrent
