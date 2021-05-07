57cf2bf7d7c4d74c0bd248fe21e8806831cdbfca
SuperNG6 <sleele@gmail.com>
2021-05-07 10:31:54 +0800

    fix arm cpu compile error

--- compile.sh
+++ compile.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+# Check CPU architecture
+ARCH=$(uname -m)
+echo -e "${INFO} Check CPU architecture ..."
+if [[ ${ARCH} == "x86_64" ]]; then
+    ARCH=" "
+elif [[ ${ARCH} == "aarch64" ]]; then
+    ARCH="--with-boost-libdir=/usr/lib/arm-linux-gnueabihf"
+elif [[ ${ARCH} == "armv7l" ]]; then
+    ARCH="--with-boost-libdir=/usr/lib/arm-linux-gnueabihf"
+else
+    echo -e "${ERROR} This architecture is not supported."
+    exit 1
+fi
+
 # get libtorrent & qbittorrent version
 QBV=$(cat ReleaseTag | head -n1)
 LIBTV=$(cat ReleaseTag | head -n 2 | tail -n 1 )
@@ -9,7 +23,7 @@ git clone https://github.com/arvidn/libtorrent.git
 cd libtorrent
 git checkout libtorrent-${LIBTV}
 ./autotool.sh
-./configure --disable-debug --enable-encryption
+./configure --disable-debug --enable-encryption ${ARCH}
 make clean && make -j$(nproc)
 make install-strip
 
